1e26a6e7cb9b5e8cd9f247c9d25ef9f49a369fb5
leeaain2025 <leeaain2025@gmail.com>
2026-09-15 13:34:13 +0900

    fix: sum_operation.py: sub_operation.py로 수정. sum 연산도 SubStrat 연산으로 코드 수정

--- src/sub_operation.py
+++ src/sub_operation.py
@@ -1,6 +1,6 @@
 from base_operation import CalculatorOperation
 
 
-class SumOperation(CalculatorOperation):
+class SubtractOperation(CalculatorOperation):
     def calculate(self, num1: float, num2: float) -> float:
-        return num1 + num2
+        return num1 - num2
