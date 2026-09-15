e248d6c44efdca0285ef3794b427937145904680
segretoo <segreto8867@gamil.com>
2026-09-15 13:44:18 +0900

    fix: add: 리뷰 반영 - docstring 작성 및 매개변수명 num1,num2로 수정

--- src/add.py
+++ src/add.py
@@ -2,6 +2,14 @@ from base_operation import CalculatorOperation
 
 
 class AddOperation(CalculatorOperation):
-    def calculate(self, a: float, b: float) -> float:
-        result = a + b
-        return result
+    def calculate(self, num1: float, num2: float) -> float:
+        """두 개의 실수를 더한 결과를 반환합니다.
+
+        Args:
+            num1 (float): 첫 번째 실수 입력값
+            num2 (float): 두 번째 실수 입력값
+
+        Returns:
+            float: num1과 num2의 합
+        """
+        return num1 + num2
